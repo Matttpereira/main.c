733f78e35bdc68d49080aaf4075dc44939288a58
GabrielNaca <gabriel.nacarelli@gmail.com>
2025-05-13 23:28:27 -0300

    Adicionei as funções 2 e 3 (do problema 2 e do problema 3)

--- main.c
+++ main.c
@@ -29,3 +29,60 @@ void executarFibonacci() {
     }
     printf("\n");
 }
+void executarFatoriais() {
+    int N;
+    printf("Digite um numero inteiro (1 a 20): ");
+    scanf("%d", &N);
+
+    if (N < 1 || N > 20) {
+        printf("Valor invalido!\n");
+        return;
+    }
+
+    long long fatoriais[20];
+    fatoriais[0] = 1;
+
+    for (int i = 1; i < N; i++) {
+        fatoriais[i] = fatoriais[i - 1] * (i + 1);
+    }
+
+    printf("Fatoriais:\n");
+    for (int i = 0; i < N; i++) {
+        printf("%d! = %lld\n", i + 1, fatoriais[i]);
+    }
+}
+
+void verificarPalindromo() {
+    char palavra[101];
+    char palavraInvertida[101];
+    int tamanho, Palindromo = 1;
+
+    printf("Digite uma palavra: ");
+    scanf("%100s", palavra);
+
+    tamanho = strlen(palavra);
+
+    for (int i = 0; i < tamanho; i++) {
+        palavraInvertida[i] = palavra[tamanho - 1 - i];
+    }
+    palavraInvertida[tamanho] = '\0';
+
+    for (int i = 0; i < tamanho; i++) {
+        char c1 = palavra[i];
+        char c2 = palavraInvertida[i];
+
+        if (c1 >= 'A' && c1 <= 'Z') c1 += 32;
+        if (c2 >= 'A' && c2 <= 'Z') c2 += 32;
+
+        if (c1 != c2) {
+            Palindromo = 0;
+            break;
+        }
+    }
+
+    if (Palindromo) {
+        printf("A palavra e um palindromo.\n");
+    } else {
+        printf("A palavra NAO e um palindromo.\n");
+    }
+}
